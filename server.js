c40f9caf51a372223438ed36a1b21210bd57e98e
ramichabchoub <ramichabchoub.rc@gmail.com>
2022-07-23 20:15:15 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+app.get("/", (req, res) => {
+        res.send("Hello World v1.0.0");
+}
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+        console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+}
+);
