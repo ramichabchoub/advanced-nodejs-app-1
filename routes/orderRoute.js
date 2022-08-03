221ebfc8b14fc9a3286dd518228cfd195413c54a
ramichabchoub <ramichabchoub.rc@gmail.com>
2022-08-03 18:52:56 +0100

    order with cash method

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 import {
         createCashOrder,
+        filterOrderForLoggedUser,
+        findAllOrders,
+        findSpecificOrder,
+        updateOrderToPaid,
+        updateOrderToDelivered,
 } from '../services/orderService.js';
 import {
         protect,
@@ -9,7 +14,25 @@ import {
 } from '../services/authService.js';
 
 const router = express.Router();
-router.use(protect, allowedTo('user'));
-router.route('/:cartId').post(createCashOrder);
+router.use(protect,);
+
+router.route('/:cartId').post(allowedTo('user'), createCashOrder);
+router.get(
+        '/',
+        allowedTo('user', 'admin', 'manager'),
+        filterOrderForLoggedUser,
+        findAllOrders
+);
+router.get('/:id', findSpecificOrder);
+router.put(
+        '/:id/pay',
+        allowedTo('admin', 'manager'),
+        updateOrderToPaid
+);
+router.put(
+        '/:id/deliver',
+        allowedTo('admin', 'manager'),
+        updateOrderToDelivered
+);
 
 export default router;
--- services/orderService.js
+++ services/orderService.js
@@ -1,6 +1,14 @@
 import asyncHandler from 'express-async-handler';
 import ApiError from '../utils/apiError.js';
 
+import {
+  getAll,
+  getOne,
+  deleteOne,
+  updateOne,
+  createOne,
+} from './handlersFactory.js';
+
 import Product from '../models/productModel.js';
 import Cart from '../models/cartModel.js';
 import Order from '../models/orderModel.js';
@@ -51,4 +59,64 @@ export const createCashOrder = asyncHandler(async (req, res, next) => {
   }
 
   res.status(201).json({ status: 'success', data: order });
+});
+
+export const filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
+  if (req.user.role === 'user') req.filterObject = { user: req.user._id };
+  next();
+});
+// @desc    Get all orders
+// @route   POST /api/v1/orders
+// @access  Protected/User-Admin-Manager
+export const findAllOrders = getAll(Order);
+
+// @desc    Get all orders
+// @route   POST /api/v1/orders
+// @access  Protected/User-Admin-Manager
+export const findSpecificOrder = getOne(Order);
+
+// @desc    Update order paid status to paid
+// @route   PUT /api/v1/orders/:id/pay
+// @access  Protected/Admin-Manager
+export const updateOrderToPaid = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(
+      new ApiError(
+        `There is no such a order with this id:${req.params.id}`,
+        404
+      )
+    );
+  }
+
+  // update order to paid
+  order.isPaid = true;
+  order.paidAt = Date.now();
+
+  const updatedOrder = await order.save();
+
+  res.status(200).json({ status: 'success', data: updatedOrder });
+});
+
+// @desc    Update order delivered status
+// @route   PUT /api/v1/orders/:id/deliver
+// @access  Protected/Admin-Manager
+export const updateOrderToDelivered = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(
+      new ApiError(
+        `There is no such a order with this id:${req.params.id}`,
+        404
+      )
+    );
+  }
+
+  // update order to paid
+  order.isDelivered = true;
+  order.deliveredAt = Date.now();
+
+  const updatedOrder = await order.save();
+
+  res.status(200).json({ status: 'success', data: updatedOrder });
 });
